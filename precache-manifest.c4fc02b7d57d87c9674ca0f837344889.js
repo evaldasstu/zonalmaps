0a77f1717cb8f5acd6266f24db68e36bebdfbb6d
Evaldas <evaldas.stulgaitis@gmail.com>
2020-04-22 16:29:34 +0300

    Updates

---
+++ precache-manifest.c4fc02b7d57d87c9674ca0f837344889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a770102e3810e38ad15737d5db29fd2",
+    "url": "/zonalmap/index.html"
+  },
+  {
+    "revision": "134c08bf6d687436bd84",
+    "url": "/zonalmap/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7cd3f98151eb09e1d542",
+    "url": "/zonalmap/static/js/2.6aec04ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zonalmap/static/js/2.6aec04ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134c08bf6d687436bd84",
+    "url": "/zonalmap/static/js/main.93cdaa5f.chunk.js"
+  },
+  {
+    "revision": "06b15df7830829ad352e",
+    "url": "/zonalmap/static/js/runtime-main.a4d940e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zonalmap/static/media/logo.5d5d9eef.svg"
+  }
+]);

37f07e4d76aedb568f56aa826f85dbdfb916cd19
Evaldas <evaldas.stulgaitis@gmail.com>
2020-04-23 15:18:14 +0300

    Add navbar and basic scaffolding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Map placeholder
-    </div>
+    <Container>
+      <Navbar expand="lg" variant="light" bg="light" className="mt-4">
+        <Navbar.Brand href="#home">Zonal Maps</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <NavDropdown title="Examples" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#examples/1">Example 1</NavDropdown.Item>
+            <NavDropdown.Item href="#examples/2">Example 2</NavDropdown.Item>
+            <NavDropdown.Item href="#examples/3">Example 3</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="https://github.com/evaldasstu/zonalmaps" className="justify-content-end">GitHub</Nav.Link>
+        </Nav>
+      </Navbar>
+      {/* <div className="App">
+      </div> */}
+    </Container>
   );
 }
 

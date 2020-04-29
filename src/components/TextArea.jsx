56e74ca0bf84f003ab96069aa25a48e2601fa966
Evaldas <evaldas.stulgaitis@gmail.com>
2020-04-29 17:09:41 +0300

    Convert TextArea to functional component and replace overlay with alert

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,62 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { LinkContainer } from 'react-router-bootstrap';
-import {
-  Card, Form, Popover, OverlayTrigger,
-} from 'react-bootstrap';
+import { Card, Form, Alert } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import './TextArea.scss';
 
-export default class TextArea extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      copySuccess: false,
-    };
-  }
+export default function TextArea(props) {
+  const [copySuccess, setCopySuccess] = useState(false);
+  let textArea = React.createRef();
 
-  copyToClipboard = () => {
-    const element = this.textArea;
-    element.select();
+  const { value } = props;
+
+  const copyToClipboard = () => {
+    textArea.select();
     document.execCommand('copy');
-    this.setState({ copySuccess: true });
-  }
+    setCopySuccess(true);
+  };
 
-  render() {
-    const { value } = this.props;
-    const { copySuccess } = this.state;
-    const popover = (
-      <Popover id="popover-basic">
-        <Popover.Content>
-          {copySuccess ? <div>Embed code copied to clipboard.</div> : null}
-        </Popover.Content>
-      </Popover>
-    );
-    return (
-      <>
-        <code>
-          <Form.Group>
-            <Form.Control
-              as="textarea"
-              rows="5"
-              ref={(teaxtArea) => { this.textArea = teaxtArea; }}
-              value={value}
-            />
-          </Form.Group>
-        </code>
-        <OverlayTrigger trigger="click" placement="right" overlay={popover} rootClose="true">
-          <LinkContainer to="#">
-            {/* Interim padding to offset overlay */}
-            <Card.Link onClick={() => this.copyToClipboard()} className="pr-2">
-              Copy to clipboard
-              <FontAwesomeIcon icon={faCopy} size="xs" />
-            </Card.Link>
-          </LinkContainer>
-        </OverlayTrigger>
-      </>
-    );
-  }
+  return (
+    <>
+      <Alert variant="success" show={copySuccess} onClose={() => setCopySuccess(false)} dismissible>
+        Embed code copied to clipboard.
+      </Alert>
+      <code>
+        <Form.Group>
+          <Form.Control
+            as="textarea"
+            value={value}
+            rows="5"
+            readOnly
+            ref={(element) => { textArea = element; }}
+          />
+        </Form.Group>
+      </code>
+      <LinkContainer to="#">
+        <Card.Link onClick={copyToClipboard}>
+          Copy to clipboard
+          <FontAwesomeIcon icon={faCopy} size="xs" />
+        </Card.Link>
+      </LinkContainer>
+    </>
+  );
 }
 
 TextArea.propTypes = {

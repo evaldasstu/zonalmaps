e00d88c760d256424e83ff0e8c390b4563ffa579
Evaldas <evaldas.stulgaitis@gmail.com>
2020-05-05 14:49:37 +0300

    Update code style and structure

--- src/App.js
+++ src/App.js
@@ -12,44 +12,42 @@ import GetEmbedCode from './components/GetEmbedCode/GetEmbedCode';
 import Embed from './components/Embed/Embed';
 import Example from './components/Example/Example';
 
-export default function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/embed/:embedParams">
-          <Embed />
-        </Route>
-        <Route path="/">
-          {/* All other routes */}
-          <Container className="my-4">
-            <Menu />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/example/:exampleNo">
-                <Example />
-              </Route>
-              <Route exact path="/embed">
-                <GetEmbedCode />
-              </Route>
-            </Switch>
-          </Container>
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+const Home = () => (
+  <>
+    <h1 className="zm-page-title">How to use</h1>
+    <Card>
+      <Card.Body>
+        <ReactMarkdown source={ReadMe} />
+      </Card.Body>
+    </Card>
+  </>
+);
 
-function Home() {
-  return (
-    <>
-      <h1 className="zm-page-title">How to use</h1>
-      <Card>
-        <Card.Body>
-          <ReactMarkdown source={ReadMe} />
-        </Card.Body>
-      </Card>
-    </>
-  );
-}
+const App = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route path="/embed/:embedParams">
+        <Embed />
+      </Route>
+      <Route path="/">
+        {/* All other routes */}
+        <Container className="my-4">
+          <Menu />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/example/:exampleNo">
+              <Example />
+            </Route>
+            <Route exact path="/embed">
+              <GetEmbedCode />
+            </Route>
+          </Switch>
+        </Container>
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default App;

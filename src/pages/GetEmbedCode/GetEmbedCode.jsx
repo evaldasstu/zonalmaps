d60e2579c5bd0344dcd0d7d00b8ae2e96daea84b
Evaldas <evaldas.stulgaitis@gmail.com>
2020-05-05 21:26:58 +0300

    Update instruction text due to changes in Google Sheets UI

--- src/pages/GetEmbedCode/GetEmbedCode.jsx
+++ src/pages/GetEmbedCode/GetEmbedCode.jsx
@@ -69,8 +69,9 @@ const GetEmbedCode = () => {
             <Link to="/#specs">specification</Link>.
           </Card.Text>
           <Card.Text>
-            Choose <strong>File</strong> &gt; <strong>Share</strong> and select{' '}
-            <strong>Get shareable link</strong> in Google Sheets to allow the spreadsheet{' '}
+            Choose <strong>File</strong> &gt; <strong>Share</strong>, select{' '}
+            <strong>Copy link</strong> and change <strong>Restricted</strong> to {' '}
+            <strong>Anyone with the link</strong> in Google Sheets to allow the spreadsheet{' '}
             to be publicly readable.
           </Card.Text>
         </Card.Body>

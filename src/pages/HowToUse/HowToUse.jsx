e4808fe5936629bd0f6331be86d60fcebe1d4718
Evaldas <evaldas.stulgaitis@gmail.com>
2020-05-09 00:29:23 +0300

    Render HTML images in README

--- src/pages/HowToUse/HowToUse.jsx
+++ src/pages/HowToUse/HowToUse.jsx
@@ -5,13 +5,14 @@ import ReactMarkdown from 'react-markdown';
 import ReadMe from '../../../README.md';
 import './HowToUse.scss';
 
+// Render heading anchors for section links
+
 function flatten(text, child) {
   return typeof child === 'string'
     ? text + child
     : Children.toArray(child.props.children).reduce(flatten, text);
 }
 
-// Render anchors for section links
 function HeadingRenderer(props) {
   const { level } = props;
   const { children } = props;
@@ -24,12 +25,30 @@ HeadingRenderer.propTypes = {
   children: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
+// Remove 'public/' from image paths
+
+function ImageRenderer(props) {
+  const { value } = props;
+  const parsedHtml = new DOMParser().parseFromString(value, 'text/html');
+  const src = parsedHtml.images[0].src.replace('public/', '');
+  const { width } = parsedHtml.images[0];
+
+  return createElement('img', { src, width, className: 'img-fluid mb-4' });
+}
+
+ImageRenderer.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
 const HowToUse = () => (
   <>
     <h1>How to use</h1>
     <Card className="how-to-use">
       <Card.Body>
-        <ReactMarkdown source={ReadMe} renderers={{ heading: HeadingRenderer }} />
+        <ReactMarkdown
+          source={ReadMe}
+          renderers={{ heading: HeadingRenderer, html: ImageRenderer }}
+        />
       </Card.Body>
     </Card>
   </>

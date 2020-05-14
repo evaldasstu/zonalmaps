bdfcae5c350c0fc70027058a309332dfb93016ec
evaldasstu <evaldasstu@users.noreply.github.com>
2020-05-14 11:20:45 +0000

    deploy: b05b32cbe51a6113e942fbd125e38693c5cd5175

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zonalmaps/precache-manifest.629a0b6366191a97ae35cffe5a01c0f6.js"
+  "/precache-manifest.6b806d5b83905ecf522bc38ec85fae9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zonalmaps/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
